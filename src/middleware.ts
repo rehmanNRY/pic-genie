1db143ead968fb596cd780d77b890773d26a3ef8
Abdul Rehman <91366959+rehmanNRY@users.noreply.github.com>
2024-10-16 22:54:46 +0500

    edit middlware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,12 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-// Make sure that the `/api/webhooks(.*)` route is not protected here
+// Skip protection for the webhook route
 export default clerkMiddleware()
-console.log("reach middlware")
 export const config = {
     matcher: [
+        // Add the path to allow the webhook
         '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+        '/api/webhooks/clerk', // Add this line
         '/(api|trpc)(.*)',
     ],
 }
