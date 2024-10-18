5be5b3af95bc56f414e422c6b6ad87525979b430
Abdul Rehman <91366959+rehmanNRY@users.noreply.github.com>
2024-10-18 20:38:20 +0500

    setup form (Zod + react-hook-form)

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -2,15 +2,17 @@ import MobileNav from '@/components/shared/MobileNav'
 import Sidebar from '@/components/shared/Sidebar'
 import React from 'react'
 
-const Layout = ({children}: {children: React.ReactNode}) => {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='root-container'>
-      <Sidebar/>
-      <MobileNav/>
-      <div className="wrapper">
-      {children}
+    <main className='root'>
+      <Sidebar />
+      <MobileNav />
+      <div className="root-container">
+        <div className="wrapper">
+          {children}
+        </div>
       </div>
-    </div>
+    </main>
   )
 }
 
